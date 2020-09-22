3f1f6f8941ac3239d1148f102441b9a0b0c16470
web-padawan <iamkulykov@gmail.com>
2020-09-22 09:31:18 +0300

    docs: create demo for GitHub pages

---
+++ src/demo/demo-snippet.ts
@@ -0,0 +1,56 @@
+import { htmlRender } from 'highlight-ts/es/render/html.js';
+import { registerLanguages } from 'highlight-ts/es/languages.js';
+import { XML } from 'highlight-ts/es/languages/xml.js';
+import { JavaScript } from 'highlight-ts/es/languages/javascript.js';
+import { init, process } from 'highlight-ts/es/process.js';
+import { defaultProps } from '../lib/components/burger.js';
+
+registerLanguages(XML, JavaScript);
+
+const highlighter = init(htmlRender);
+
+type BurgerKnobs = {
+  name: string;
+  value: string | number | null;
+};
+
+const INDENT = '  ';
+
+export class DemoSnippet extends HTMLElement {
+  constructor() {
+    super();
+    const root = this.attachShadow({ mode: 'open' });
+    root.appendChild(document.createElement('slot'));
+  }
+
+  update(element: string, knobs: BurgerKnobs[]): void {
+    let markup = `<script type="module">\n${INDENT}import 'vanilla-hamburger/${element}.js';\n</script>\n`;
+
+    markup += `<${element}`;
+
+    const attrs = knobs.filter(({ name, value }) => value && value != defaultProps[name]);
+
+    attrs
+      .sort((a, b) => (a.name > b.name ? 1 : -1))
+      .forEach(({ name, value }) => {
+        markup += `\n${INDENT}${name}="${value}"`;
+      });
+
+    markup += `${attrs.length ? '\n' : ''}></${element}>`;
+
+    const { value } = process(highlighter, markup, ['xml', 'js']);
+
+    const code = this.querySelector('code');
+    if (code) {
+      code.innerHTML = value;
+    }
+  }
+}
+
+customElements.define('demo-snippet', DemoSnippet);
+
+declare global {
+  interface HTMLElementTagNameMap {
+    'demo-snippet': DemoSnippet;
+  }
+}
